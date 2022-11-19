cd5037283d4d1ac72088067b280a30077ad33a5c
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-11-19 16:23:33 +0300

    Реализованы все задачи

--- Core/CarDealershipsSystem.Application/Interfaces/IBranchService.cs
+++ Core/CarDealershipsSystem.Application/Interfaces/IBranchService.cs
@@ -4,9 +4,8 @@ namespace CarDealershipsSystem.Application.Interfaces
 {
     public interface IBranchService
     {
-        public interface IBranchRepository
-        {
-            IEnumerable<Branch> GetBranches();
-        }
+       
+        IEnumerable<Branch> GetBranches();
+        
     }
 }
