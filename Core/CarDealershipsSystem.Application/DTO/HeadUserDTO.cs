e98f3ce94c47ba9d761d45da2eb85ee58f5c4472
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-12-15 13:45:09 +0300

    Добавил DTO классы для новых отношений

---
+++ Core/CarDealershipsSystem.Application/DTO/HeadUserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace CarDealershipsSystem.Application.DTO
+{
+    public class HeadUserDTO
+    {
+        public int HeadId { get; set; }
+
+        public string HeadPassword { get; set; } = null!;
+
+        public string HeadLogin { get; set; } = null!;
+
+        public long HeadPassData { get; set; }
+    }
+}
